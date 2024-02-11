35ff737679453cac4a35edff9b1a4f2017ce5dd4
Julien <perona.julien@yahoo.fr>
2024-02-11 10:42:54 +0100

    last commit

--- database/unique_items._repository.js
+++ database/unique_items._repository.js
@@ -1,5 +1,6 @@
 const { Op } = require("sequelize");
 var UniqueItems = require('./Models/uniques_items')
+var namesDict = require('./Models/namesDict')
 
 module.exports = { 
 
@@ -16,7 +17,14 @@ module.exports = {
     async getAllUniqueArmors() {
         let result
         try {
-            result = await UniqueItems.findAll( { where: { kind: "Armor" }} )       
+            result = await UniqueItems.findAll( { where: { kind: "Armor" }} )
+            
+            // Exemple traduction des noms
+            /*result.forEach(element => {
+                if(namesDict.names.get(element.name) != undefined ) {
+                    element.name = namesDict.names.get(element.name)                    
+                }
+            });    */   
         } catch (error) {
             console.log(error)    
         }        
